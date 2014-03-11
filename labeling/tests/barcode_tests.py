d28c5b3f9016bed308d0fde214b0eafdfa157030
erik van widenfelt <ew2789@gmail.com>
2014-03-11 22:04:32 +0200

    label classes and tests

--- labeling/tests/barcode_tests.py
+++ labeling/tests/barcode_tests.py
@@ -1,3 +1,5 @@
+import os.path
+
 from django.test import TestCase
 
 from ..classes import Label
@@ -32,11 +34,13 @@ class BarcodeTests(TestCase):
         label = Label()
         label.zpl_template = label.default_template
         self.assertTrue(label.write_formatted_label_to_file())
+        self.assertTrue(os.path.isfile(label.file_name))
 
     def test_label_printer1(self):
         """Assert can determine the label printer if have a default printer."""
         label = Label()
         label.zpl_template = label.default_template
+        label.default_label_printer = self.label_printer_default
         self.assertIsNotNone(label.label_printer)
         self.assertEquals(label.label_printer.cups_server_ip, '127.0.0.1')
 
@@ -45,26 +49,59 @@ class BarcodeTests(TestCase):
         label = Label()
         label.zpl_template = label.default_template
         label.client_addr = '127.0.0.2'
+        label.default_label_printer = self.label_printer_default
         self.assertTrue(label.label_printer, self.label_printer_default)
         self.assertFalse('Unable to determine the label printer for client {0}'.format(label.client_addr) in label.message)
 
     def test_label_printer3(self):
         """Assert for a given client address, cannot determine the label printer if there isn't a default."""
         self.label_printer_default.default = False
+        self.label_printer_default.cups_server_ip = '127.0.0.127'
         self.label_printer_default.save()
+        self.label_printer_client_10.default = False
+        self.label_printer_client_10.save()
         self.label_printer_client_11_default.default = False
         self.label_printer_client_11_default.save()
         label = Label()
         label.zpl_template = label.default_template
         label.client_addr = '127.0.0.2'
+        label.default_label_printer = None
         self.assertIsNone(label.label_printer)
-        self.assertTrue('Unable to determine the label printer for client {0}'.format(label.client_addr) in label.message)
+
+    def test_label_printer3a(self):
+        """Assert for a given client address, picks localhost label printer if there isn't a default."""
+        self.label_printer_default.default = False
+        self.label_printer_default.save()
+        self.label_printer_client_10.default = False
+        self.label_printer_client_10.save()
+        self.label_printer_client_11_default.default = False
+        self.label_printer_client_11_default.save()
+        label = Label()
+        label.zpl_template = label.default_template
+        label.client_addr = '127.0.0.2'
+        label.default_label_printer = None
+        self.assertEquals(label.label_printer, self.label_printer_default)
+
+    def test_label_printer3b(self):
+        """Assert for a given client address, does not picks localhost label printer if there is a default."""
+        self.label_printer_default.default = False
+        self.label_printer_default.save()
+        self.label_printer_client_10.default = False
+        self.label_printer_client_10.save()
+        self.label_printer_client_11_default.default = False
+        self.label_printer_client_11_default.save()
+        label = Label()
+        label.zpl_template = label.default_template
+        label.client_addr = '127.0.0.2'
+        label.default_label_printer = self.label_printer_client_11
+        self.assertEquals(label.label_printer, self.label_printer_client_11)
 
     def test_label_printer4a(self):
         """Assert for a given client address, can determine the label printer for the client."""
         label = Label()
         label.zpl_template = label.default_template
         label.client_addr = '127.0.0.10'
+        label.default_label_printer = self.label_printer_default
         self.assertTrue(label.label_printer == self.label_printer_client_10)
 
     def test_label_printer4b(self):
@@ -72,6 +109,7 @@ class BarcodeTests(TestCase):
         label = Label()
         label.zpl_template = label.default_template
         label.client_addr = '127.0.0.13'
+        label.default_label_printer = self.label_printer_default
         self.assertEqual(label.label_printer, self.label_printer_default)
 
     def test_label_printer4c(self):
@@ -79,6 +117,7 @@ class BarcodeTests(TestCase):
         label = Label()
         label.zpl_template = label.default_template
         label.client_addr = '127.0.0.11'
+        label.default_label_printer = self.label_printer_default
         self.assertTrue(label.label_printer == self.label_printer_client_11_default)
 
     def test_label_printer5(self):
@@ -86,4 +125,5 @@ class BarcodeTests(TestCase):
         label = Label()
         label.zpl_template = label.default_template
         label.client_addr = '127.0.0.10'
+        label.default_label_printer = self.label_printer_default
         self.assertTrue(label.label_printer == self.label_printer_client_10)
