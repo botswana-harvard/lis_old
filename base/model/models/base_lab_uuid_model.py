161c99fb13a662a44204cf3b5cb9478193aebe44
SirOne Pharatlhatlhe <sirpharatlhatlhe@yahoo.co.uk>
2014-06-20 15:15:46 +0200

    getting aliquots to be serialized if sync is in intalled apps

--- base/model/models/base_lab_uuid_model.py
+++ base/model/models/base_lab_uuid_model.py
@@ -1,4 +1,7 @@
-from edc.base.model.models import BaseUuidModel
+try:
+    from edc.device.sync.models import BaseSyncUuidModel as BaseUuidModel
+except ImportError:
+    from edc.base.model.models import BaseUuidModel
 
 
 class BaseLabUuidModel(BaseUuidModel):
