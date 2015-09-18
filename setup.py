388d13e8bc0816c1af0bbf1a80e88856fa07dcca
erikvw <ew2789@gmail.com>
2015-09-18 08:37:47 +0200

    fix edc_base imports

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='lis',
-    version='1.7.1',
+    version='1.7.1dev',
     author=u'Erik van Widenfelt',
     author_email='ew2789@gmail.com',
     packages=find_packages(),
